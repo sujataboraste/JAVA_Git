e158b4dfd06c76793dd6f20e731d21fd6fe17040
Shree <sujata.boraste@gmail.com>
2026-04-26 02:28:33 +0530

    This is a last

---
+++ JAVA_Git/src/git_JAVA/Last.java
@@ -0,0 +1,12 @@
+package git_JAVA;
+
+public class Last {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome in the final stage");
+
+	}
+
+}
