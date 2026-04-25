2e22ec90c9951c47906cb956e22865eaa3cc4d05
Shree <sujata.boraste@gmail.com>
2026-04-25 14:07:56 +0530

    I_am_in_28July_Batch

---
+++ JAVA_Git/src/git_JAVA/Second_git.java
@@ -0,0 +1,12 @@
+package git_JAVA;
+
+public class Second_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Everyone");
+
+	}
+
+}
